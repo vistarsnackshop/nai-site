d4635663966f0a8ade616ae5c245525708a94a56
Dallon DeLoach <dallon.deloach@pfgc.com>
2024-12-02 10:07:40 -0700

    web.config and server.js change

--- server.js
+++ server.js
@@ -1,38 +1,21 @@
-const { createServer } = require("http");
-const { parse } = require("url");
-const next = require("next");
-
-const dev = process.env.NODE_ENV !== "production"; // Determines if it's in dev or production mode
-const port = 3000; // Explicitly set the port for your app to listen on
-
-const hostname = "localhost"; // Ensure this matches your hostname
-const app = next({ dev, hostname, port });
-const handle = app.getRequestHandler();
-
+import { createServer } from 'http'
+import { parse } from 'url'
+import next from 'next'
+ 
+const port = parseInt(process.env.PORT || '3000', 10)
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = app.getRequestHandler()
+ 
 app.prepare().then(() => {
-  createServer(async (req, res) => {
-    try {
-      const parsedUrl = parse(req.url, true);
-      const { pathname, query } = parsedUrl;
-
-      if (pathname === "/a") {
-        await app.render(req, res, "/a", query);
-      } else if (pathname === "/b") {
-        await app.render(req, res, "/b", query);
-      } else {
-        await handle(req, res, parsedUrl);
-      }
-    } catch (err) {
-      console.error("Error occurred handling", req.url, err);
-      res.statusCode = 500;
-      res.end("internal server error");
-    }
-  })
-    .once("error", (err) => {
-      console.error(err);
-      process.exit(1);
-    })
-    .listen(port, async () => {
-      console.log(`> Ready on http://localhost:${port}`);
-    });
-});
+  createServer((req, res) => {
+    const parsedUrl = parse(req.url, true)
+    handle(req, res, parsedUrl)
+  }).listen(port)
+ 
+  console.log(
+    `> Server listening at http://localhost:${port} as ${
+      dev ? 'development' : process.env.NODE_ENV
+    }`
+  )
+})

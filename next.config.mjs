f00bbf7a42ce50757f88ac92f62ad3407dc81e44
Dallon DeLoach <dallon.deloach@pfgc.com>
2024-12-06 12:43:09 -0700

    web.config and server.js change

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  basePath: "/nai",
   experimental: {
     appDir: true,
-    serverComponentsExternalPackages: ["odbc"], // Ensure 'odbc' is installed
+    serverComponentsExternalPackages: ["odbc"],
   },
-  basePath: '/nai', // Set base path if app is served under a subdirectory
 };
 
 export default nextConfig;
